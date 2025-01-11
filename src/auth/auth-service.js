b3fe5ce24746cd9a91fa09d2eb7004e054037f7f
billc2013 <williamchurch17@gmail.com>
2025-01-11 00:18:08 -0500

    Update auth-service.js

---
+++ src/auth/auth-service.js
@@ -0,0 +1,31 @@
+# /src/auth/auth-service.js
+export const getUserInfo = async (accessToken) => {
+    console.log('Getting user info...');
+    try {
+        const response = await fetch(`https://${auth0Config.domain}/userinfo`, {
+            headers: { 
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('Response status:', response.status);
+        const userInfo = await response.json();
+        console.log('User info received:', userInfo);
+        return userInfo;
+    } catch (error) {
+        console.error('Error getting user info:', error);
+        throw error;
+    }
+};
+
+export const initiateLogin = () => {
+    console.log('Login button clicked');
+    const auth0LoginUrl = `https://${auth0Config.domain}/authorize?` +
+        `response_type=token&` +
+        `client_id=${auth0Config.clientId}&` +
+        `redirect_uri=${encodeURIComponent(auth0Config.redirectUri)}&` +
+        `scope=${auth0Config.scope}`;
+    
+    console.log('Auth0 URL:', auth0LoginUrl);
+    window.location.href = auth0LoginUrl;
+};
