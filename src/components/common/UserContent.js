917f0c901152f6585e62f5cbbca4a85232d3d944
billc2013 <williamchurch17@gmail.com>
2025-01-11 00:19:01 -0500

    Update UserContent.js

---
+++ src/components/common/UserContent.js
@@ -0,0 +1,25 @@
+# /src/components/common/UserContent.js
+export class UserContent extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+            <div id="user-content" style="display: none;">
+                <textarea id="user-text" rows="4" cols="50"></textarea>
+                <button id="save-text">Save Text</button>
+            </div>
+        `;
+        this.setupSaveHandler();
+    }
+
+    async setupSaveHandler() {
+        const { saveUserText } = await import('../../firebase/firestore-service.js');
+        this.querySelector('#save-text').onclick = async () => {
+            const text = this.querySelector('#user-text').value;
+            const userEmail = localStorage.getItem('userEmail'); // We'll need to store this during login
+            await saveUserText(userEmail, text);
+            alert('Text saved!');
+        };
+    }
+}
+
+customElements.define('user-content', UserContent);
