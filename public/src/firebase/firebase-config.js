0c03633f2c0c5bbc8d9b60821cd2227dfdc6a128
billc2013 <williamchurch17@gmail.com>
2025-01-27 23:42:45 -0500

    updating firebase config

--- public/src/firebase/firebase-config.js
+++ public/src/firebase/firebase-config.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
-  
+    apiKey: "AIzaSyC5nQrhR3-Z8NCCtSbOa25mkcC3-8KHQCc",
+    authDomain: "webr-1de0a.firebaseapp.com",
+    projectId: "webr-1de0a",
+    storageBucket: "webr-1de0a.firebasestorage.app",
+    messagingSenderId: "845544523689",
+    appId: "1:845544523689:web:dfce9e291c1829f527e6b8"
+};
